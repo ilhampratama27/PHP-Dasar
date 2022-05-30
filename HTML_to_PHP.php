62cd290a24c9688cfb0a5ed1ef2a138cf19071fe
ilhampratama27 <educationilham27@gmail.com>
2022-05-30 22:35:13 +0700

    PHP Dasar

---
+++ HTML_to_PHP.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $nama = 'ilham';
+    $umur = '17';
+    $pesan = 'stay halal brother';
+    ?>
+
+    <p><?php echo "Nama saya adalah $nama, Sekarang saya berumur $umur, saya mempunyai pesan $pesan." ?></p>
+</body>
+</html>
