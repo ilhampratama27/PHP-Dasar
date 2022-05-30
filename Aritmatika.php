2c8221c2729184abbfe4cc2f523a0601ce7bb2ad
ilhampratama27 <educationilham27@gmail.com>
2022-05-30 23:41:12 +0700

    File baru

---
+++ Aritmatika.php
@@ -0,0 +1,14 @@
+   <?php
+$var_a = 4;
+$var_b = 8;
+$var_c = 10;
+
+$hasil = $var_a + $var_b;
+echo "Hasil tambah $var_a dan $var_b adalah $hasil <br>"
+
+;
+
+$hasilterbaru = $hasil * $var_c;
+echo "kemudian hasil dari tambah tersebut akan dikalikan menjadi $var_c maka hasilnya $hasilterbaru "
+?>
+
